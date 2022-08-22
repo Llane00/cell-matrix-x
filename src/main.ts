9726c90d683512e86118c0f552dab23fc1692f1c
Llane00 <bluetinestyle@126.com>
2022-08-22 22:47:15 +0800

    chore: init

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+// import { createApp } from 'vue'
+// import './style.css'
+// import App from './App.vue'
+
+// createApp(App).mount('#app')
+
+import * as PIXI from 'pixi.js';
+console.log(PIXI);
+
